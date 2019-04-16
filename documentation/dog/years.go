522eaa340950f82ab8249a5c142848d48001c7ed
Diego Felipe <diegof.fnogueira@gmail.com>
2019-04-15 22:04:17 -0300

    Documentation exercise

---
+++ documentation/dog/years.go
@@ -0,0 +1,7 @@
+//Package dog allows us to more fully understand dog
+package dog
+
+//Years converts human years to dog years
+func Years(human int) int {
+	return human * 7
+}
--- documentation/main.go
+++ documentation/main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
+	"course-of-go/documentation/dog"
 	"course-of-go/documentation/mymath"
 	"fmt"
 )
+
+type cachorro struct {
+	name string
+	age int
+}
 
 func main() {
 
 	fmt.Println("A soma de 2 e 3 é:", mymath.Sum(2, 3))
 	fmt.Println("A soma de 10 e 20 e 30 é:", mymath.Sum(10, 20, 30))
 	fmt.Println("A soma de 2 e 3 e 4 e 5 é:", mymath.Sum(2, 3, 4, 5))
+
+	fmt.Println("")
+	fmt.Println("**************************")
+
+	lili := cachorro{
+		name: "Lyli",
+		age:dog.Years(10),
+	}
+	fmt.Println("A idade do animal é:", lili.age)
 
 }
